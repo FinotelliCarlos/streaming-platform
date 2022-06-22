24d92cb6cd64bfec31fd0d1a8dd792e1ce8a4e99
FinotelliCarlos <carlos.finotelli@outlook.com>
2022-06-22 10:35:37 -0300

    clean project to start app

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,3 @@
-import { gql, useQuery } from '@apollo/client'
-
-interface Lesson {
-  id: string
-  title: string
-}
-
-const GET_LECTURE_QUERY = gql`
-  query {
-    lessons {
-      id
-      title
-    }
-  }
-`
-
 export const App = () => {
-  const { data } = useQuery<{ lessons: Lesson[] }>(GET_LECTURE_QUERY)
-
-  return (
-    <ul>
-      {data?.lessons.map(lessons => {
-        return <li key={lessons.id}>{lessons.title}</li>
-      })}
-    </ul>
-  )
+  return <div>Hello</div>
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+
 import { App } from './App'
-import '../src/styles/global.css'
-import { ApolloProvider } from '@apollo/client'
+
 import { client } from './lib/apollo'
+import { ApolloProvider } from '@apollo/client'
+
+import '../src/styles/global.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
