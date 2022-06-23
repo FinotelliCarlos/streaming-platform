0e5eede4579d5f43701afa9af39649ff567d942d
FinotelliCarlos <carlos.finotelli@outlook.com>
2022-06-23 15:02:19 -0300

    feat: router with slug props and video props

--- src/main.tsx
+++ src/main.tsx
@@ -3,15 +3,10 @@ import ReactDOM from 'react-dom/client'
 
 import { App } from './App'
 
-import { client } from './lib/apollo'
-import { ApolloProvider } from '@apollo/client'
-
 import '../src/styles/global.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ApolloProvider client={client}>
-      <App />
-    </ApolloProvider>
+    <App />
   </React.StrictMode>
 )
